eefd979c961eda2fe7ad573abd96b3612e537d13
Nasnapier <97366228+Nasnapier@users.noreply.github.com>
2022-05-19 23:16:53 -0400

    Delete theme directory

--- theme/assets/bsify.media_queries.js
+++
@@ -1,5 +0,0 @@
-require('./_polyfills.js').getComputedStyle();
-
-module.exports.size = function () {
-  return window.getComputedStyle(document.body,':after').getPropertyValue('content').replace(/['"]/g, "");
-};
